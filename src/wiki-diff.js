635d2d7857e3f2bbf3ed0fc5ced47e8735fa3e73
Max Klein <isalix@gmail.com>
2019-02-22 17:04:29 -0800

    more fiiling in, but how will i set innerhtml of TRs and also react in there.

--- src/wiki-diff.js
+++ src/wiki-diff.js
@@ -3,35 +3,49 @@ import React, {Component} from 'react';
 class WikiDiff extends Component {
     render() {
         const innerHTMLObj = {__html: this.props.diffObj.diffHTML};
-        return (<div className="cs-wiki-diff">
-                <div className="mediawiki ltr sitedir-ltr mw-hide-empty-elt ns-0 ns-subject mw-editable skin-vector action-view">
-                <div className="body" id="content" role="main">
-                <div id="bodyContent" className="mw-body-content diff diff-contentalign-left">
-                <DiffTitle newold="new" diffObj={this.props.diffObj}>
+        return <div className="cs-wiki-diffmediawiki ltr sitedir-ltr mw-hide-empty-elt ns-0 ns-subject mw-editable skin-vector action-view">
+                                                <DiffTitle newold="new" diffObj={this.props.diffObj}>
                 </DiffTitle>
-                 <DiffTitle newold="old" diffObj={this.props.diffObj}>
+                <DiffTitle newold="old" diffObj={this.props.diffObj}>
                 </DiffTitle>
-                <table
-                           data-mw="interface"
-                           dangerouslySetInnerHTML={innerHTMLObj} />
-                </div>
-                </div>
-                </div>
-                </div>)
+            <table className="mw-body-content diff diff-contentalign-left">
+                <colgroup>
+                    <col className="diff-marker"></col>
+                        <col className="diff-content"></col>
+                            <col className="diff-marker"></col>
+                                <col className="diff-content"></col>
+                </colgroup>
+                <tbody
+                    data-mw="interface"
+                    dangerouslySetInnerHTML={innerHTMLObj} />
+
+            </table>
+        </div>
     }
 }
 
 class DiffTitle extends Component {
 
     render(){
+        const oOrn = this.props.newold === 'new' ? "n" : "o"
         const revIdKey = this.props.newold + "RevId"
         const revDateKey = this.props.newold + "RevDate"
         const revUserKey = this.props.newold + "RevUser"
         const revCommentKey = this.props.newold + "RevComment"
         const revId = this.props.diffObj[revIdKey]
-        console.log(revIdKey)
-        return <div>{revId}</div>
+        const revDate = this.props.diffObj[revDateKey]
+        const revUser = this.props.diffObj[revUserKey]
+        const revComment = this.props.diffObj[revCommentKey]
+
+        return <tr className="diff-title">
+            <td className={"diff-"+oOrn+"title"} colSpan={2}>
+            <div id={"mw-diff-"+oOrn+"title1"}>
+                <strong>{revUser}</strong>
+            </div>
+            </td>
+        </tr>
     }
 }
+
 
 export default WikiDiff
